2d74f65614ac2153ef9f31829126a1428d81eb73
Grigory Zubankov <Grigory.Zubankov@acronis.com>
2018-11-03 10:03:04 +0300

    add: readme

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # logf
-Fast and async logger in Go.
+
+[![GoDoc](https://godoc.org/github.com/ssgreg/logf?status.svg)](https://godoc.org/github.com/ssgreg/logf)
+[![Build Status](https://travis-ci.org/ssgreg/logf.svg?branch=master)](https://travis-ci.org/ssgreg/logf)
+[![Go Report Status](https://goreportcard.com/badge/github.com/ssgreg/logf)](https://goreportcard.com/report/github.com/ssgreg/logf)
+[![Coverage Status](https://coveralls.io/repos/github/ssgreg/logf/badge.svg?branch=master)](https://coveralls.io/github/ssgreg/logf?branch=master)
+
+Faster-than-light, asynchronous, structured logger in Go with zero allocation count.
+
+## Example
+
+The following example creates the new `logf` logger and logs a message.
+
+```go
+package main
+
+import (
+    "runtime"
+
+    "github.com/ssgreg/logf"
+)
+
+func main() {
+    // The default channel writer writes to stdout using json encoder.
+    writer, writerClose := logf.NewChannelWriter.Default()
+    defer writerClose()
+
+    logger := logf.NewLogger(logf.LevelInfo, writer)
+
+    logger.Info("got cpu info", logf.Int("count", runtime.NumCPU()))
+}
+```
+
+The output is the following:
+
+```json
+{"level":"info","ts":"2018-11-03T09:49:56+03:00","msg":"got cpu info","count":8}
+```
--- examples/basic/main.go
+++ examples/basic/main.go
@@ -1,116 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"io/ioutil"
-	"os"
-	"sync"
-	"time"
+	"runtime"
 
 	"github.com/ssgreg/logf"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
-func newLogger(l logf.Level, w io.Writer) (*logf.Logger, logf.ChannelWriterCloseFunc) {
-	ew, close := logf.NewChannelWriter.Default()
-
-	return logf.NewLogger(l, ew), close
-}
-
-var messages = makePseudoMessages(1000)
-
-func makePseudoMessages(n int) []string {
-	messages := make([]string, n)
-	for i := range messages {
-		messages[i] = fmt.Sprintf("A text that pretend to be a real message in case of length %d", i)
-	}
-	return messages
-}
-
-func getMessage(n int) string {
-	return messages[n%len(messages)]
-}
-
-func newZapLogger(lvl zapcore.Level) *zap.Logger {
-	c := zap.NewProductionConfig()
-	c.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-	c.DisableCaller = true
-	c.OutputPaths = []string{"stdout"}
-	c.Sampling = nil
-
-	logger, _ := c.Build()
-	return logger
-
-	// ec := zap.NewProductionEncoderConfig()
-	// // ec.EncodeDuration = zapcore.NanosDurationEncoder
-	// // ec.EncodeTime = zapcore.EpochNanosTimeEncoder
-	// ec.EncodeTime = zapcore.ISO8601TimeEncoder
-	// enc := zapcore.NewJSONEncoder(ec)
-	// return zap.New(zapcore.NewCore(
-	// 	enc,
-	// 	&Discarder{},
-	// 	lvl,
-	// ))
-}
-
-// A Syncer is a spy for the Sync portion of zapcore.WriteSyncer.
-type Syncer struct {
-	err    error
-	called bool
-}
-
-// SetError sets the error that the Sync method will return.
-func (s *Syncer) SetError(err error) {
-	s.err = err
-}
-
-// Sync records that it was called, then returns the user-supplied error (if
-// any).
-func (s *Syncer) Sync() error {
-	s.called = true
-	return s.err
-}
-
-// Called reports whether the Sync method was called.
-func (s *Syncer) Called() bool {
-	return s.called
-}
-
-// A Discarder sends all writes to ioutil.Discard.
-type Discarder struct{ Syncer }
-
-// Write implements io.Writer.
-func (d *Discarder) Write(b []byte) (int, error) {
-	// return 0, errors.New("ficj")
-	return ioutil.Discard.Write(b)
-	// fmt.Println(string(b))
-	// panic(33)
-	// return 0, nil
-}
-
 func main() {
-	// tmp := make([]byte, 0, 1024*1024*200)
-	logger, close := newLogger(logf.LevelDebug, os.Stdout) // bytes.NewBuffer(tmp))
-	defer close()
+	// The default channel writer writes to stdout using json encoder.
+	writer, writerClose := logf.NewChannelWriter.Default()
+	defer writerClose()
 
-	// logger := newZapLogger(zap.DebugLevel)
-	// defer logger.Sync()
+	logger := logf.NewLogger(logf.LevelInfo, writer)
 
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-
-	for g := 0; g < 1; g++ {
-		go func() {
-			defer wg.Done()
-
-			for i := 0; i < 1; i++ {
-				logger.Info(getMessage(i), logf.Bytes("test", []byte(`1), logf.Int("test", 1), logf.Int("test", 1)`)))
-				time.Sleep(time.Second)
-			}
-		}()
-	}
-
-	wg.Wait()
+	logger.Info("got cpu info", logf.Int("count", runtime.NumCPU()))
 }
