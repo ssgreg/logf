c698b1fc0600b9efcfa0801af6b831ff38414071
Grigory Zubankov <Grigory.Zubankov@acronis.com>
2018-11-08 18:11:36 +0300

    add: NO_COLOR to disable colored text logs

--- escape_code.go
+++ escape_code.go
@@ -1,5 +1,9 @@
 package logf
 
+import (
+	"os"
+)
+
 // EscapeCode represents a single code in the ANSI escape sequences.
 //
 // These sequences define functions that change display graphics, control
@@ -11,6 +15,18 @@ package logf
 // and left-bracket characters specify an alphanumeric code that controls
 // a keyboard or display function.
 type EscapeCode int8
+
+// checkNoColor checks for NO_COLORS environment variable to disable color
+// output.
+//
+// All command-line software which outputs text with ANSI color added should
+// check for the presence of a NO_COLOR environment variable that, when
+// present (regardless of its value), prevents the addition of ANSI color.
+func checkNoColor() bool {
+	_, ok := os.LookupEnv("NO_COLOR")
+
+	return ok
+}
 
 // Text colors.
 const (
@@ -77,6 +93,11 @@ const (
 // AtEscapeSequence calls the given fn, wrapped with the escape sequence,
 // based on the given code.
 func AtEscapeSequence(buf *Buffer, clr EscapeCode, fn func()) {
+	if noColor {
+		fn()
+		return
+	}
+
 	buf.AppendString("\x1b[")
 	AppendInt(buf, int64(clr))
 	buf.AppendByte('m')
@@ -87,6 +108,11 @@ func AtEscapeSequence(buf *Buffer, clr EscapeCode, fn func()) {
 // AtEscapeSequence2 calls the given fn, wrapped with the escape sequence,
 // based on the given codes.
 func AtEscapeSequence2(buf *Buffer, clr1, clr2 EscapeCode, fn func()) {
+	if noColor {
+		fn()
+		return
+	}
+
 	buf.AppendString("\x1b[")
 	AppendInt(buf, int64(clr1))
 	buf.AppendByte(';')
@@ -99,6 +125,11 @@ func AtEscapeSequence2(buf *Buffer, clr1, clr2 EscapeCode, fn func()) {
 // AtEscapeSequence3 calls the given fn, wrapped with the escape sequence,
 // based on the given codes.
 func AtEscapeSequence3(buf *Buffer, clr1, clr2, clr3 EscapeCode, fn func()) {
+	if noColor {
+		fn()
+		return
+	}
+
 	buf.AppendString("\x1b[")
 	AppendInt(buf, int64(clr1))
 	buf.AppendByte(';')
@@ -109,3 +140,7 @@ func AtEscapeSequence3(buf *Buffer, clr1, clr2, clr3 EscapeCode, fn func()) {
 	fn()
 	buf.AppendString("\x1b[0m")
 }
+
+var (
+	noColor = checkNoColor()
+)
