50ea1c71c9e64f461c32a66c10a6c36b26cf47cb
Grigory Zubankov <Grigory.Zubankov@acronis.com>
2018-11-12 18:13:49 +0300

    add: EnableSyncOnError to config for ChannelWriter

--- channel.go
+++ channel.go
@@ -6,12 +6,31 @@ import (
 	"sync"
 )
 
+// ChannelWriterConfig allows to configure ChannelWriter.
 type ChannelWriterConfig struct {
-	Capacity      int
-	Appender      Appender
+	// Capacity specifies the underlying channel capacity.
+	Capacity int
+
+	// Appender specified the basic Appender for all Entries.
+	//
+	// Default Appender is WriterAppender with JSON Encoder.
+	Appender Appender
+
+	// ErrorAppender specifies the Appender for errors returning by basic
+	// Appender.
+	//
+	// Default ErrorAppender does nothing.
 	ErrorAppender Appender
+
+	// EnableSyncOnError specifies whether Appender.Sync should be called
+	// for messages with LevelError or not.
+	//
+	// Default value is false.
+	EnableSyncOnError bool
 }
 
+// WithDefaults returns the new config in which all uninitialized fields are
+// filled with their default values.
 func (c ChannelWriterConfig) WithDefaults() ChannelWriterConfig {
 	// Chan efficiency depends on the number of CPU installed in the system.
 	// Tests shows that min chan capacity should be twice as big as CPU count.
@@ -31,6 +50,7 @@ func (c ChannelWriterConfig) WithDefaults() ChannelWriterConfig {
 	return c
 }
 
+// NewChannelWriter returns a new ChannelWriter with the given config.
 var NewChannelWriter = channelWriterGetter(
 	func(cfg ChannelWriterConfig) (EntryWriter, ChannelWriterCloseFunc) {
 		l := &channelWriter{}
@@ -133,7 +153,11 @@ func (l *channelWriter) append(e Entry) {
 	// This allows to commit buffered messages in case of futher unexpected
 	// panic or crash.
 	if e.Level <= LevelError {
-		l.sync()
+		if l.EnableSyncOnError {
+			l.sync()
+		} else {
+			l.flush()
+		}
 	}
 }
 
