2f12a3e72220da9aa000ffa8a902e1431ce4976b
Grigory Zubankov <Grigory.Zubankov@acronis.com>
2018-11-16 09:17:40 +0300

    add: error comments

--- error.go
+++ error.go
@@ -2,8 +2,13 @@ package logf
 
 import "fmt"
 
+// ErrorEncoder is the function type to encode the given error.
 type ErrorEncoder func(string, error, FieldEncoder)
 
+// DefaultErrorEncoder encodes the given error as a set of fields.
+//
+// A mandatory field with the given key and an optional field with the
+// full verbose error message.
 func DefaultErrorEncoder(k string, e error, m FieldEncoder) {
 	var msg string
 	if e == nil {
